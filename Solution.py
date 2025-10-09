f754fc472fb3631a191defcecd7191fedea5bf68
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2025-10-08 21:43:27 -0600

    Valid solution

--- Solution.py
+++ Solution.py
@@ -1,11 +1,27 @@
 class Solution(object):
     def mergeAlternately(self, word1, word2):
-        """
-        :type word1: str
-        :type word2: str
-        :rtype: str
-        """
-        return word1 + word2
+        
+        #It starts with the word1, and then goes with word2
+        solution = ''
+        size1 = len(word1)
+        size2 = len(word2)
+        
+        for i in range(min(size1, size2)):
+            solution += word1[i] + word2[i]
+        '''
+        if len(word2) > size:
+            solution += word2[size:]'''
+        
+        if size2 > size1:
+            solution += word2[size1:]
+        elif size1 > size2:
+            solution += word1[size2:]
+        else:
+            pass
+        #if the word2 is bigger than word1, the final letters
+        #will go till the end
+
+        return solution
 
 def main():
     word1 = input("input word1:     \n")
