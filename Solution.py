45e48dd197093719acd8f7bfa988ef36c98807f7
Carlos Paredes <carlos.paredes6900@alumnos.udg.mx>
2025-10-17 21:26:26 -0600

    Saving the solution of the reaching points

--- Solution.py
+++ Solution.py
@@ -1,8 +1,29 @@
 class Solution(object):
-    pass
+
+    def canReach(x1, y1, x2, y2):
+        visited = set()
+
+        def dfs(x, y):
+            if (x, y) in visited:
+                return False
+            if x > x2 or y > y2:
+                return False
+            if x == x2 and y == y2:
+                return True
+
+            visited.add((x, y))
+            return dfs(x + y, y) or dfs(x, x + y)
+
+        return "Yes" if dfs(x1, y1) else "No"
+
 
 def main():
     print("Hello World.\n")
+
+    solution = Solution()
+
+    print(f"\n  case 1:     1, 1: Starting in 3, 5. Solution must be \'Yes\'.\n")
+    print(f'\n  Solution case 1:    {solution.canReach(1, 1, 3, 5)}')
 
 if __name__ == '__main__':
     main()
